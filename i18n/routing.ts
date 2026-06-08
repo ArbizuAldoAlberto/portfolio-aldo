eb2ab1645ba6ca474f4f65c61ab337aeca77d8ae
ALDO ALBERTO ARBIZU <arbizualdoalberto@gmail.com>
2026-06-08 18:34:19 -0300

    Fix: force locale prefix and simplify middleware to resolve 404

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,1 +1,10 @@
-import {defineRouting} from 'next-intl/routing'; import {createNavigation} from 'next-intl/navigation'; export const routing = defineRouting({ locales: ['en', 'es'], defaultLocale: 'es' }); export const {Link, redirect, usePathname, useRouter, getPathname} = createNavigation(routing);
+﻿import {defineRouting} from 'next-intl/routing';
+import {createNavigation} from 'next-intl/navigation';
+
+export const routing = defineRouting({
+  locales: ['en', 'es'],
+  defaultLocale: 'es',
+  localePrefix: 'always' // Forzamos a que siempre use /es o /en
+});
+
+export const {Link, redirect, usePathname, useRouter, getPathname} = createNavigation(routing);
